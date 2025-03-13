692ba19eb69e24c42b641a75605957558d9d6e7e
David Liu <david-khala@hotmail.com>
2025-03-14 00:23:01 +0800

    add new relic

---
+++ py/tests/test_new_relic.py
@@ -0,0 +1,13 @@
+import unittest
+
+from davidkhala.newrelic.log import Ingestion
+
+
+class LogIngestionTestCase(unittest.TestCase):
+    def test_send(self):
+        i = Ingestion()
+        i.send("hello world")
+
+
+if __name__ == '__main__':
+    unittest.main()
